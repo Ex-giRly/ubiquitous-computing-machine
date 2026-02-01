4ddb495334381149111ae777bd705845cdcc6297
ALBA-Girly <Albaumeh@gmail.com>
2026-02-01 19:34:40 +0100

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,12 @@
+empty_list = []
+print()
+
+numbers = [1,2, 3, 4, 5, 6 ,7, 8, 9]
+print(numbers)
+
+triple = [4, 5 , 8] * 3
+print(triple)
+
+alist = [100, 3200, 455, 675, 156, 456]
+alist = alist[::-1]
+print(alist, "\n")
